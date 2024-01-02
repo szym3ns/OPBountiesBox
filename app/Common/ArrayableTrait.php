548e429cc9429966cd4e45a0ad17a48e0d7f90d5
Szymon Horbal <horbalszymon@gmail.com>
2024-01-02 22:28:38 +0100

    Fixed enums in ArrayableTrait

--- app/Common/ArrayableTrait.php
+++ app/Common/ArrayableTrait.php
@@ -4,17 +4,25 @@ declare(strict_types=1);
 
 namespace App\Common;
 
+use ReflectionClass;
+
 trait ArrayableTrait
 {
     public function toArray(): array
     {
+        $reflection = new ReflectionClass($this);
         $result = [];
 
-        foreach ($this as $property => $value) {
-            if ($value instanceof ArrayableInterface) {
-                $result[$property] = $value->toArray();
+        foreach ($reflection->getProperties() as $property) {
+            $propertyName = $property->getName();
+            $propertyValue = $property->getValue($this);
+
+            if ($propertyValue instanceof ArrayableInterface) {
+                $result[$propertyName] = $propertyValue->toArray();
+            } elseif (is_object($propertyValue) && enum_exists($propertyValue::class)) {
+                $result[$propertyName] = $propertyValue->value;
             } else {
-                $result[$property] = $value;
+                $result[$propertyName] = $propertyValue;
             }
         }
 
